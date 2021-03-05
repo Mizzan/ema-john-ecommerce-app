904b2fe256123459a6c9246685d09a704bba1c69
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-05 19:39:51 +0600

    added react router functionality and added few components

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+const NotFound = () => {
+  return (
+    <div>
+      <h4>Ohhhh NOOOOO..</h4>
+      <h1>Your page is not found</h1>
+      <h4>404 Error!!!</h4>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Review.css";
+const Review = () => {
+  return (
+    <div>
+      <h1>This is order review</h1>
+    </div>
+  );
+};
+
+export default Review;
